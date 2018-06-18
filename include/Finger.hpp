a296767a752736fe732db588dd9fddd670c9f839
Kevin Balz <kevin.balz@web.de>
2018-06-19 01:26:52 +0200

    Basic Read & Write

---
+++ include/Finger.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace feel
+{
+    enum Finger
+    {
+        HAND_0_THUMB_0,
+        HAND_0_THUMB_1,
+        HAND_0_INDEX_0,
+        HAND_0_INDEX_1,
+        HAND_0_MIDDLE_0,
+        HAND_0_MIDDLE_1,
+        HAND_0_RING_0,
+        HAND_0_RING_1,
+        HAND_0_PINKY_0,
+        HAND_0_PINKY_1,
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "Finger.hpp"
+#define ASIO_STANDALONE
+#include "asio.hpp"
 
 int main()
 {
     std::cout << "Hello Feel" << std::endl;
+    asio::io_service io;
+    asio::serial_port serial(io, "/dev/tty.usbmodem1D151");
+    serial.set_option(asio::serial_port::baud_rate(9600));
+    serial.set_option(asio::serial_port::character_size(8));
+    char c = '\0';
+    std::string f = "1337\n";
+    std::cout << f  << std::endl;
+    asio::write(serial, asio::buffer(f, f.length()));
+    while (c != EOF)
+    {
+        asio::read(serial, asio::buffer(&c, 1));
+        std::cout << c << std::endl;
+    }
+
     return 0;
 }
